360a6529011501b1c595bb8dc373cccb735dd632
Sriram Srininivasan <anushamsriram@gmail.com>
2019-04-10 21:09:25 +0530

    Create HelloWorld.go

---
+++ Go/HelloWorld.go
@@ -0,0 +1,15 @@
+// Main Package
+package main
+
+//Typical Import Statement With Different Syntax
+import (
+	"fmt" //this pacakge has formatting functions
+)
+
+//main is the execution point for our go program
+func main() {
+	fmt.Println("hello World") //prints "Hello World" message to the console. 
+}
+
+/* If you have written "C" program apart from package and import statement, nothing much has changed. 
+GO is a language that is created with "C" language as an inspiration. 
