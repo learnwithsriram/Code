df2c1d9eb7356fd2ffb12f81fff057783198fe42
Sriram Srininivasan <anushamsriram@gmail.com>
2019-04-11 17:00:36 +0530

    Create values.go

---
+++ Go/values.go
@@ -0,0 +1,23 @@
+package main
+
+/*
+
+Go has value types like integer, strings, floats, booleans,
+this code example demonstrates this concept
+Author : Sriram Srinivasan
+www.learnwithsriram.com
+sriram@learnwithsriram.com
+*/
+
+import (
+	"fmt" //for formatting funtions
+)
+
+func main() {
+
+	fmt.Println("This is a String")
+	fmt.Println("This is an integer", 10)
+	fmt.Println("This is a float : ", 5.0/2.0)
+	fmt.Println("This is a boolean : ", true)
+
+}
